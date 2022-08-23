9365c795a77d302dcbcce67a8f2025df707093a7
Blake Stickland <blakelists@yahoo.com.au>
2022-08-24 08:57:09 +1000

    intiial commit

---
+++ todo-app-frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TodoApp from "./components/todo/TodoApp";
+
+const App = () => {
+  return (
+    <div className="App">
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
