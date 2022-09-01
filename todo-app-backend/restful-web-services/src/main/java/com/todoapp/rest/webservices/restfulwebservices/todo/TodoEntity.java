b5516602e9f08e66da6df49b7ac99ebc9ee02a7b
Blake Stickland <blakelists@yahoo.com.au>
2022-09-01 23:10:33 +1000

    crud methods work in sql database -- endpoints worked with postman calls for all crud methods -- need to connect to frontend

---
+++ todo-app-backend/restful-web-services/src/main/java/com/todoapp/rest/webservices/restfulwebservices/todo/TodoEntity.java
@@ -0,0 +1,31 @@
+//package com.todoapp.rest.webservices.restfulwebservices.todo;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//
+//@Entity
+//@Table(name = "todos")
+//public class TodoEntity {
+//	
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+//	private long id;
+//	
+//	@Column(name = "description")
+//	private String description;
+//	
+//	@Column(name = "targetDate")
+//	private String targetDate;
+//	
+//	@Column(name = "done")
+//	private boolean done;
+//	
+//	public TodoEntity() {
+//		
+//	}
+//
+//}
